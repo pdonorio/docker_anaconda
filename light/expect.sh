d60a1eaa8fed71ad33c879e954d6b89e625486c4
paulie <tempo.pause@gmail.com>
2014-11-12 12:45:16 +0100

    Modified the light script. Looks much better now. This makes also work the ipython notebook ;)

--- light/expect.sh
+++ light/expect.sh
@@ -1,1 +1,18 @@
-../full/expect.sh
+#!/usr/bin/expect -f
+
+# a long timeout, to wait for the installation to complete without exiting
+set timeout 120
+
+# launch command
+spawn bash /opt/installer.sh
+# accept license, press q, say yes
+expect -re "Welcome*"
+send "yes\rqyes\r"
+# ask path to install
+expect "*conda\\] >>>"
+send "/opt/miniconda/\r"
+# put the path inside bashrc for root user
+expect "*no\\] >>>"
+send "yes\r"
+
+interact
